3e21dff20d5874f4c145b6bd9cbc8d474cd728e4
mikhail <mlozanov@gmail.com>
2011-10-27 19:36:49 +0300

    submission test added

---
+++ subtest.sh
@@ -0,0 +1,1 @@
+python tools/playgame.py --engine_seed 42 --player_seed 42 --food none --end_wait=0.25 --verbose --log_dir game_logs --turns 30 --map_file tools/submission_test/test.map "./MyBot" "python tools/submission_test/TestBot.py" --nolaunch -e --strict --capture_errors
