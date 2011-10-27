3e6e7bb2ce78bacd11e4b791ef46841bd0a6b3af
mikhail <mlozanov@gmail.com>
2011-10-27 22:38:02 +0300

    1 unit radius version of pathFind, map changed to bigger and more interesting, .gitignore updated with mac specific files

--- MyBot.hs
+++ MyBot.hs
@@ -41,7 +41,7 @@ generateOrders (a : xs) tp w gp gs = (generateOrders xs newTp w gp gs) ++ [tempO
   where food = foodTiles gs
         antPos = pointAnt a
         closestFood = closestPoint gp food 435246523 antPos (0,0)
-        tempOrder = (Order a) $ directionTo antPos closestFood
+        tempOrder = (Order a) $ directionTo antPos (pathFind gp gs (viewradius2 gp) closestFood a) --closestFood
         newTp = if isValidOrder tp w tempOrder
           then Set.insert (move (directionOrder tempOrder) (pointAnt $ a)) tp
           else tp
@@ -57,10 +57,27 @@ closestPoint gp (f:fs) d ap p
 --buildGraph :: [Point] -> Ant -> Int -> Graph.Graph
 --buildGraph (f:fs) a i = Graph.buildG (0,10::Graph.Vertex) []
 
-astar :: GameParams -> GameState -> Point -> Ant -> [Point]
-astar gp gs target a = dest
-    where dest = [target]
-          h = distance gp (pointAnt a) target
+pathFind :: GameParams -> GameState -> Int -> Point -> Ant -> Point
+pathFind _ _ 0 _ _ = (0,0)
+pathFind gp gs radius target ant = fst $ findMinDist ds (head ds)
+    where antPos = pointAnt ant
+
+          w = world gs
+
+          possiblePaths = map (fm antPos) possibleDirections
+              where possibleDirections = filter (\dir -> passable w (Order ant dir)) [North .. West]
+                    fm = flip move
+
+          ds = map (\p -> (p, distance gp p target)) possiblePaths
+
+          findMinDist :: [(Point, Int)] -> (Point, Int) -> (Point, Int)
+          findMinDist [] (mp, md) = (mp, md)
+          findMinDist ((p,d):ps) (mp, md) 
+              | md < d = findMinDist ps (mp, md)
+              | otherwise = findMinDist ps (p, d)
+
+          pp = pathFind gp gs (radius - 1) target ant
+
 
 doTurn :: GameParams -> GameState -> IO [Order]
 doTurn gp gs = do
@@ -68,10 +85,10 @@ doTurn gp gs = do
     -- 
   let mine = myAnts $ ants gs  
       generatedOrders = generateOrders mine (Set.empty) (world gs) gp gs
-  -- this shows how to check the remaining time
 
---  elapsedTime <- timeRemaining gs
---  hPutStrLn stderr $ show elapsedTime
+  -- this shows how to check the remaining time
+  --  elapsedTime <- timeRemaining gs
+  --  hPutStrLn stderr $ show elapsedTime
   -- wrap list of orders back into a monad
   return generatedOrders
 -- | This runs the game
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python tools/playgame.py "./MyBot" "python tools/sample_bots/python/RandomBot.py" --map_file tools/maps/example/tutorial1.map --log_dir game_logs --turns 60 --scenario --food none --player_seed 7 --verbose -e
+python tools/playgame.py "./MyBot" "python tools/sample_bots/python/RandomBot.py" --map_file tools/maps/random_walk/random_walk_02p_02.map --log_dir game_logs --turns 200  --player_seed 7 -e
